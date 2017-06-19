c96d9266e42d9da6a03cfbfa70762c64fa58990f
joeb15 <joebanko15@gmail.com>
2017-06-18 20:21:33 -0700

    Only allow high priority news

--- NewsData.py
+++ NewsData.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
-import requests, IO
+import IO
+import requests
 
 
 def get_data():
@@ -15,7 +16,7 @@ def __get_headlines(page):
     pages = page.split("<")
     news_storage=""
     for curr in pages:
-        if curr.__contains__("item-heading item-heading_"):
+        if curr.__contains__("item-heading item-heading_hi"):
             title=curr.split(">")[1]
             title = title.replace("&#x27;","'").replace("&amp;","&").strip()
             if not news_storage.__contains__(title):
